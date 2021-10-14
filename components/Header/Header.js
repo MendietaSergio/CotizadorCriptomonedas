ff9ca27a45c89fc0218b661fae071f5eed493a1b
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-14 10:29:50 -0300

    Añade mas estilos al header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -11,6 +11,13 @@ export default Header;
 const styles = StyleSheet.create({
     encabezado :{
         paddingTop: Platform.OS === 'ios' ? 50 : 25,
-        fontFamily: 'Lato-Black'
+        fontFamily: 'Lato-Black',
+        backgroundColor:"#5E49E2",
+        paddingBottom:10,
+        textAlign:'center',
+        textTransform:'uppercase',
+        fontSize:20,
+        color:"#FFF",
+        marginBottom:30
     }
 })
