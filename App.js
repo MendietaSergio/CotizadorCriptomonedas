b37662573fc01f96b41cc6224874d2eb9093433a
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-14 10:24:36 -0300

    añade importacion de fuente Lato-Black

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
+import * as Font from "expo-font"
 import { 
   StyleSheet, 
   Text, 
@@ -6,15 +7,33 @@ import {
   StatusBar,
   SafeAreaView
 } from 'react-native';
+import Header from './components/Header/Header';
 
 export default function App() {
+  // forma de importar fuente externa. 
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  useEffect(() =>{
+    if(!fontsLoaded){
+      loadFonts();
+    }
+  })
+  const loadFonts = async ( ) =>{
+    await Font.loadAsync({
+      'Lato-Black': require('./assets/fonts/Lato-Black.ttf')
+    })
+    setFontsLoaded(true)
+  }
   return (
     <>
-
+    <Text style={styles.prueba}>Prueba de Lato Black</Text>
+      <Header/>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  prueba: {
+    fontFamily:"Lato-Black"
+  }
   
 });
