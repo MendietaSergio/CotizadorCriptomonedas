df1a1d51a1fe468a2d7225064881f3933aa09efe
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-15 10:13:23 -0300

    Añade componente 'Cotizacion', añade estados nuevos

--- App.js
+++ App.js
@@ -7,10 +7,16 @@ import {
 } from 'react-native';
 import Header from './components/Header/Header';
 import Formulario from './components/Formulario/Formulario';
+import axios from "axios"
+import Cotizacion from './components/Cotizacion/Cotizacion';
 
 export default function App() {
   // forma de importar fuente externa. 
   const [fontsLoaded, setFontsLoaded] = useState(false);
+  const [moneda, setMoneda] = useState('')
+  const [criptomoneda, setCriptomoneda] = useState('')
+  const [consultarAPI, setConsultarAPI] = useState(false)
+  const [resultado, setResultado] = useState({})
   useEffect(() =>{
     if(!fontsLoaded){
       loadFonts();
@@ -22,6 +28,22 @@ export default function App() {
     })
     setFontsLoaded(true)
   }
+  //esto se ejecuta cuando consultarAPI es true
+  useEffect(() =>{
+    const cotizarCriptomoneda = async () =>{
+
+      if(consultarAPI){
+        console.log("listo para cotizar");
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+        const resultado = await axios.get(url)
+        //guardo el resultado que con las monidas que necesito.
+        setResultado(resultado.data.DISPLAY[criptomoneda][moneda])
+        console.log(resultado.data.DISPLAY[criptomoneda][moneda]);
+        setConsultarAPI(false)
+      }
+    }
+    cotizarCriptomoneda()
+  },[consultarAPI])
   return (
     <>
       <Header/>
@@ -30,7 +52,16 @@ export default function App() {
         source={require('./assets/img/cryptomonedas.png')}
       />
       <View style={styles.contenido}>
-        <Formulario/>
+        <Formulario
+          moneda={moneda}
+          criptomoneda={criptomoneda}
+          setCriptomoneda={setCriptomoneda}
+          setMoneda={setMoneda}
+          setConsultarAPI={setConsultarAPI}
+        />
+        <Cotizacion 
+          resultado={resultado}
+        />
       </View>
     </>
   );
