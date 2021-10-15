bca0b734d2f715cf268a1e8c664491f4e5baedf7
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-15 10:19:40 -0300

    Añade estilos a los resultados

--- components/Cotizacion/Cotizacion.js
+++ components/Cotizacion/Cotizacion.js
@@ -8,7 +8,7 @@ const Cotizacion = ({
     if(Object.keys(resultado).length === 0) return null;
     return(
         <View style={styles.resultado}>
-            <Text style={styles.texto}>
+            <Text style={[styles.texto, styles.precio]}>
                 <Text style={styles.span}> {resultado.PRICE}</Text>
             </Text>
             <Text style={styles.texto}>Precio más alto del día: {''}
@@ -29,16 +29,21 @@ const Cotizacion = ({
 
 const styles = StyleSheet.create({
     resultado: {
-
+        backgroundColor:'#5E49E2',
+        padding:20,
+        marginTop:20
     },
     texto: {
-
+        color:'#FFF',
+        fontFamily:'Lato-Regular',
+        fontSize:18,
+        marginBottom:10
     },
     precio: {
-
+        fontSize:38
     },
     span:{
-
+        fontFamily: 'Lato-Black'
     }
 })
 
